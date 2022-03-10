9c9e3ae523fde3e68d5b3b4e8687613e70d1d955
Alex Widener <awidener3@gmail.com>
2022-03-10 09:25:06 -0500

    added missing params from User type in typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,8 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-	type User {
-		_id: ID
+	# Inputs
+	input UserInput {
 		first_name: String
 		last_name: String
 		username: String
@@ -10,17 +10,41 @@ const typeDefs = gql`
 		password: String
 	}
 
+	# Types
+	type User {
+		_id: ID
+		first_name: String
+		last_name: String
+		username: String
+		email: String
+		password: String
+		weight: Int
+		height: Int
+		age: Int
+		gender: String
+		exercises: [Exercise]
+		exercise_goal: Int
+		mindful_sessions: [Mindfulness]
+		mindful_goal: Int
+		water_intake: [Water]
+		water_goal: Int
+		meals: [Meal]
+		calorie_goal: Int
+	}
+
 	type Auth {
 		token: ID!
 		user: User
 	}
 
+	# Queries
 	type Query {
 		users: [User]!
 		user(userId: ID!): User
 		me: User
 	}
 
+	# Mutations
 	type Mutation {
 		addUser(
 			first_name: String!
