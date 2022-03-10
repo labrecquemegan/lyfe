edf9e00cf030c1d11ed32348d0a4fa56b5f48393
xndrbrgs <aborgescolon@gmail.com>
2022-03-10 10:04:36 -0500

    added new files

---
+++ client/src/components/NavBar/nav.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../images/png/logo-white.png';
+import './style.scss';
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#exercise">Exercises</a></p>
+          <p><a href="#nutrition">Nutrition</a></p>
+          <p><a href="#mindfulness">Mindfulness</a></p>
+          <p><a href="#water">Water Intake</a></p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign Up</p>
+        <button type="button">Sign In</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <p><a href="#exercise">Exercises</a></p>
+            <p><a href="#nutrition">Nutrition</a></p>
+            <p><a href="#mindfulness">Mindfulness</a></p>
+            <p><a href="#water">Water Intake</a></p>
+          </div>
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <p>Sign Up</p>
+            <button type="button">Sign In</button>
+          </div>
+        </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,13 +1,73 @@
-import './App.css';
+import React, { useState } from 'react'
+import { useEffect } from "react";
+import { useRef } from "react";
 
-import TopNav from '../components/TopNav/topNav';
+import './App.scss'
 
-function App() {
+// import Navbar from '../components/NavBar/nav'
+import Landing from '../pages/LandingPage/Landing';
+
+// import SideBar from '../components/SideBar'
+
+import useLocoScroll from "../hooks/useLocoScroll";
+
+const App = () => {
+  const ref = useRef(null);
+  const [preloader, setPreload] = useState(true);
+
+
+  useLocoScroll(!preloader);
+
+  useEffect(() => {
+    if (!preloader && ref) {
+      if (typeof window === "undefined" || !window.document) {
+        return;
+      }
+    }
+  }, [preloader]);
+
+  const [timer, setTimer] = React.useState(0);
+
+  const id = React.useRef(null);
+
+  const clear = () => {
+    window.clearInterval(id.current);
+    setPreload(false);
+  };
+
+  React.useEffect(() => {
+    id.current = window.setInterval(() => {
+      setTimer((time) => time - 1);
+    }, 1000);
+    return () => clear();
+  }, []);
+
+  React.useEffect(() => {
+    if (timer === 0) {
+      clear();
+    }
+  }, [timer]);
+
+  if (typeof window === "undefined" || !window.document) {
+    return null;
+  }
+
   return (
     <>
-      <TopNav />
+      {preloader ? (
+        <div className="loader-wrapper absolute">
+        </div>
+      ) : (
+        <div
+          className="main-container"
+          id="main-container"
+          data-scroll-container
+          ref={ref}
+        >
+          <Landing />
+        </div>
+      )}
     </>
   );
-}
-
+};
 export default App;
