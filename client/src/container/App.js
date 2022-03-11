b5cb034ceafa95698c84a58961033be876caf027
Alex Widener <awidener3@gmail.com>
2022-03-11 11:00:48 -0500

    added apollo client to app.js

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,71 +1,73 @@
-import React, { useState } from 'react'
-import { useEffect } from "react";
-import { useRef } from "react";
-import useLocoScroll from "../hooks/useLocoScroll";
+import React, { useState, useEffect, useRef } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import useLocoScroll from '../hooks/useLocoScroll';
 
-import './App.css'
+import './App.css';
 
-// Pages 
+// Pages
 
 import LandingPage from '../pages/LandingPage/landing';
 
+const client = new ApolloClient({
+	uri: '/graphql',
+	cache: new InMemoryCache(),
+});
+
 const App = () => {
-  const ref = useRef(null);
-  const [preloader, setPreload] = useState(true);
+	const ref = useRef(null);
+	const [preloader, setPreload] = useState(true);
 
+	useLocoScroll(!preloader);
 
-  useLocoScroll(!preloader);
+	useEffect(() => {
+		if (!preloader && ref) {
+			if (typeof window === 'undefined' || !window.document) {
+				return;
+			}
+		}
+	}, [preloader]);
 
-  useEffect(() => {
-    if (!preloader && ref) {
-      if (typeof window === "undefined" || !window.document) {
-        return;
-      }
-    }
-  }, [preloader]);
+	const [timer, setTimer] = React.useState(0);
 
-  const [timer, setTimer] = React.useState(0);
+	const id = React.useRef(null);
 
-  const id = React.useRef(null);
+	const clear = () => {
+		window.clearInterval(id.current);
+		setPreload(false);
+	};
 
-  const clear = () => {
-    window.clearInterval(id.current);
-    setPreload(false);
-  };
+	React.useEffect(() => {
+		id.current = window.setInterval(() => {
+			setTimer((time) => time - 1);
+		}, 1000);
+		return () => clear();
+	}, []);
 
-  React.useEffect(() => {
-    id.current = window.setInterval(() => {
-      setTimer((time) => time - 1);
-    }, 1000);
-    return () => clear();
-  }, []);
+	React.useEffect(() => {
+		if (timer === 0) {
+			clear();
+		}
+	}, [timer]);
 
-  React.useEffect(() => {
-    if (timer === 0) {
-      clear();
-    }
-  }, [timer]);
+	if (typeof window === 'undefined' || !window.document) {
+		return null;
+	}
 
-  if (typeof window === "undefined" || !window.document) {
-    return null;
-  }
-
-  return (
-    <>
-      {preloader ? (
-        <div className="loader-wrapper absolute">
-        </div>
-      ) : (
-        <div
-          className="main-container"
-          id="main-container"
-          data-scroll-container
-          ref={ref}
-        >
-          <LandingPage />
-        </div>
-      )}
-    </>
-  );
+	return (
+		<ApolloProvider client={client}>
+			{preloader ? (
+				<div className="loader-wrapper absolute"></div>
+			) : (
+				<div
+					className="main-container"
+					id="main-container"
+					data-scroll-container
+					ref={ref}
+				>
+					<LandingPage />
+				</div>
+			)}
+		</ApolloProvider>
+	);
 };
 export default App;
