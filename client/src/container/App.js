6d02dbb6136136008bb28f5a715b9f26298925df
Alex Widener <awidener3@gmail.com>
2022-03-11 11:05:33 -0500

    removed unnecessary code

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,6 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import useLocoScroll from '../hooks/useLocoScroll';
+import React from 'react';
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import './App.css';
 
@@ -8,66 +13,31 @@ import './App.css';
 
 import LandingPage from '../pages/LandingPage/landing';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
 	uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem('id_token');
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache(),
 });
 
 const App = () => {
-	const ref = useRef(null);
-	const [preloader, setPreload] = useState(true);
-
-	useLocoScroll(!preloader);
-
-	useEffect(() => {
-		if (!preloader && ref) {
-			if (typeof window === 'undefined' || !window.document) {
-				return;
-			}
-		}
-	}, [preloader]);
-
-	const [timer, setTimer] = React.useState(0);
-
-	const id = React.useRef(null);
-
-	const clear = () => {
-		window.clearInterval(id.current);
-		setPreload(false);
-	};
-
-	React.useEffect(() => {
-		id.current = window.setInterval(() => {
-			setTimer((time) => time - 1);
-		}, 1000);
-		return () => clear();
-	}, []);
-
-	React.useEffect(() => {
-		if (timer === 0) {
-			clear();
-		}
-	}, [timer]);
-
-	if (typeof window === 'undefined' || !window.document) {
-		return null;
-	}
-
 	return (
 		<ApolloProvider client={client}>
-			{preloader ? (
-				<div className="loader-wrapper absolute"></div>
-			) : (
-				<div
-					className="main-container"
-					id="main-container"
-					data-scroll-container
-					ref={ref}
-				>
-					<LandingPage />
-				</div>
-			)}
+			<LandingPage />
 		</ApolloProvider>
 	);
 };
+
 export default App;
