e5ff9fcb8c99491fcb0cf9abfa84d93006ccb6c2
unknown <javi.mariscal13@gmail.com>
2022-03-12 14:56:26 -0500

    Added basic animation

---
+++ client/src/components/waterAnimation/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./waterAnimation.css";
+
+const Water = () => {
+	return (
+
+		<div class = "parent">
+        <div class = "child"></div>
+         </div>
+
+	);
+};
+
+export default Water;
