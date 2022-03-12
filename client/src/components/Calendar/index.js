034e1f802825d044e710eafa87dd61f8ba16c12e
ML <labrecquemegan@hotmail.com>
2022-03-11 23:21:29 -0500

    made the calendar better, still needs to go down the page and take in data from user

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,101 +1,146 @@
-import React from 'react';
-import moment from 'moment';
-
+import React from "react";
+import moment from "moment";
+import ReactDOM from "react-dom";
 
 const dayNames = {
-    1: 'S',
-    2: 'M',
-    3: 'T',
-    4: 'W',
-    5: 'R',
-    6: 'F',
-    7: 'S'
-}
+  0: "S",
+  1: "M",
+  2: "T",
+  3: "W",
+  4: "R",
+  5: "F",
+  6: "S",
+};
+
 
 // changes color of cell based on workload
-function Fill({ date, alpha }) {
-    let style = {
-        backgroundColor: `rgba(0, 256, 0, ${alpha})`
-    };
+function Cell({ date, alpha }) {
+  let style = {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    height: '20px',
+    width: '20px',
+    border: '1px solid rgba(0,0,0,0.1)',
+    margin: '2px',
+    borderRadius: '50px',
+    backgroundColor: `rgba(0, 256, 0, ${alpha})`,
+  };
 
-    return (
-        <div className='timeline-cells-fill' style={style}></div>
-    )
+  return <div className="timeline-cells-fill" style={style}></div>;
 }
 
 // reads the months
 function Month({ startDate, index }) {
-    let date = moment(startDate.add(index * 7, 'day'));
-    let monthName = date.format('MMM');
+  let date = moment(startDate.add(index * 5, "day"));
+  let monthName = date.format("MMM");
 
-    return (
-        <div className={`timeline-months-month $monthName`}>
-            {monthName}
-        </div>
-    )
+  let style = {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    height: '#{(15 + 2) * 8}px',
+  }
+  return (
+    <div className='timeline-months-month' style={style}>{monthName}</div>
+  );
 }
 
 // will show the weeks that include the 3 weekday names
 function WeekDay({ index }) {
-    return (
-        <div className='timeline-weekdays-weekday'>
-        {dayNames[index]}
-        </div>
-    )
+    let style = {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        height: '#{(15 + 2) * 8}px',
+        margin: '1%',
+        justifyContent: 'space-evenly',
+        width: '20px'
+      }
+
+  return <div className="timeline-weekdays-weekday" style={style}>{dayNames[index]}</div>;
 }
 
+function Timeline({ range, data }) {
+  let days = Math.abs(range[0].diff(range[1], "days"));
+  let cells = Array.from(new Array(days));
+  let weekDays = Array.from(new Array(7));
+  let months = Array.from(new Array(Math.floor(days/7)));
 
+  let min = Math.min(0, ...data.map((d) => d.value));
+  let max = Math.max(...data.map((d) => d.value));
 
+  let colorMultiplier = 1 / (max - min);
 
-function Timeline({ range,data }) {
-    
-    let days = Math.abs(range[0].diff(range[1], 'days'));
-    let cells = Array.from(new Array(days));
-    let weekDays = Array.from(new(7));
-    let months = Array.from(new Array(Math.floor(days / 7)));
+  let startDate = range[0];
+  let dayFormat = "DDMMYYY";
 
-    let min = Math.min(0, ...data.map(d => d.value));
-    let max = Math.max(...data.map(d => d.value));
+  let style = {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignContent: 'center',
+    alignItems: 'center',
+    // justifyContent: 'space-evenly',
+    height: 'height: #{(15 + 2) * 8}px',
+    maxWidth: '175px'
+  }
 
-    let colorMultiplier = 1 / (max-min);
+  let timelineStyle = {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    // marginTop: '100px'
+  }
 
-    let startDate = range[0];
-    let dayFormat = 'DDMMYYY'
+  return (
+    <div className="timeline" style={timelineStyle}>
+      {/* <div className="timeline-months">
+        {months.map((_, index) => (
+          <Month key={index} index={index + 1} startDate={startDate} />
+        ))}
+      </div> */}
 
-    return (
-        <div className='timeline'>
-
-            <div className='timeline-months'>
-                {months.map(( _, index ) => <Month key={index} index={index} startDate={startDate} />)}
-            </div>   
-
-            <div className='timeline-body'>
-
-                <div className='timeline-weekDays'>
-                    {weekDays.map(( _, index) => <WeekDay key={index} index={index} startDate={startDate} />)}
-                </div>    
-
-                <div className='timeline-cells'>
-                    {cells.map(( _, index) => {
-                        let date = moment(startDate).add(index, 'day');
-                        let dataPoint = data.find(d => moment(date).format(dayFormat) === moment(d.date).format(dayFormat));
-                        let alpha = colorMultiplier * dataPoint.value;
-
-                        return (
-                            <Fill
-                                key={index}
-                                index={index}
-                                date={date}
-                                alpha={alpha}
-                            />
-                        );
-                        }
-                    )}
-                </div>
-            </div>
+      <div className="timeline-body">
+        <div className="timeline-weekDays" style={style}>
+          {weekDays.map((_, index) => (
+            <WeekDay key={index} index={index} startDate={startDate} />
+          ))}
         </div>
-    )}
 
+        <div className="timeline-cells" style={style}>
+          {cells.map((_, index) => {
+            let date = moment(startDate).add(index, "day");
+            let dataPoint = data.find(
+              (d) =>
+                moment(date).format(dayFormat) ===
+                moment(d.date).format(dayFormat)
+            );
+            let alpha = colorMultiplier * dataPoint.value;
 
+            return <Cell key={index} index={index} date={date} alpha={alpha} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  // 1 year range
+  let startDate = moment().add(-30, 'days');
+  let dateRange = [startDate, moment()];
+
+  let data = Array.from(new Array(30)).map((_, index) => {
+    return {
+      date: moment(startDate).add(index, "day"),
+      value: Math.floor(Math.random() * 100),
+    };
+  });
+  return <Timeline range={dateRange} data={data} />;
+}
+
+const Calendar = () => {
+    return <App />;
+};
 
 export default Calendar;
--- client/src/pages/water.js
+++ client/src/pages/water.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import Calendar from 'react-github-contribution-calendar';
+// import ReactDOM from 'react-dom';
+// import Calendar from 'react-github-contribution-calendar';
+import Calendar from '../components/Calendar/index';
 
-var values = {
-  '2016-06-23': 1,
-  '2016-06-26': 2,
-  '2016-06-27': 3,
-  '2016-06-28': 4,
-  '2016-06-29': 4
-}
-var until = '2016-06-30';
+// var values = {
+//   '2016-06-23': 1,
+//   '2016-06-26': 2,
+//   '2016-06-27': 3,
+//   '2016-06-28': 4,
+//   '2016-06-29': 4
+// }
+// var until = '2016-06-30';
 
 
 
@@ -21,11 +22,9 @@ const Water = () => {
     <>
     <div className='page'>
       <div className='container'>
-        <div >
-        <p><Calendar values={values} until={until} /></p>
+        <Calendar  />
       
           <p>tehbhjewbicwe</p>
-        </div>
       </div>
     </div>
     </>
