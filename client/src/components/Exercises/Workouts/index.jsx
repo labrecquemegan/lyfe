9a2839e733b4f91cf07879728359b2beec8c54fb
xndrbrgs <aborgescolon@gmail.com>
2022-03-12 21:45:21 -0500

    changed some css props

--- client/src/components/Exercises/Workouts/index.jsx
+++ client/src/components/Exercises/Workouts/index.jsx
@@ -3,36 +3,33 @@ import "./workout.scss";
 
 export default function Workouts() {
   return (
-    <section className="main">
-      <div className="container">
+    <section className="workouts-container">
         <div className="row">
           <h2>Workouts</h2>
             <div class="skills" id="skills" data-scroll-section>
               <div>
-                <h1 data-scroll>Skills</h1>
                 <div className="skills-tree" data-scroll>
                   <div className="skill">
                     <div className="skill-title">
-                      <h2>Javascript</h2>
+                      <h2>Beginner</h2>
                       <p>Frontend Development and Single-Page Apps</p>
                     </div>
                   </div>
                   <div className="skill">
                     <div className="skill-title">
-                      <h2>Tools</h2>
+                      <h2>Intermediate</h2>
                       <p>Design Systems and Smart Interfaces</p>
                     </div>
                   </div>
                   <div className="skill">
                     <div className="skill-title">
-                      <h2>Backend</h2>
+                      <h2>Difficult</h2>
                       <p>Content Management Systems</p>
                     </div>
                   </div>
                 </div>
               </div>
           </div>
-        </div>
       </div>
     </section>
   );
