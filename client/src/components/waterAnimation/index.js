a4f751fedaed0366c011e0f8180152f94228719c
unknown <javi.mariscal13@gmail.com>
2022-03-14 11:13:32 -0400

    Commenting out moving

--- client/src/components/waterAnimation/index.js
+++ client/src/components/waterAnimation/index.js
@@ -2,35 +2,18 @@ import React from "react";
 import "./waterAnimation.css";
 
 
+
 const Water = () => {
 	return (
-
-		<div class = "gauge">
-        <div class = "gauge__body">
-            <div class = "gauge__fill"></div>
-            <div class = "gauge__cover"></div>
-        </div>
-    </div>
-
+		<div>
+			<div className = "gauge">
+        		<div className = "gauge__body">
+            		<div className = "gauge__fill"></div>
+            		<div className = "gauge__cover"></div>
+        		</div>
+    		</div>
+		</div>
 	);
 };
-
-const gaugeElement = document.querySelector(".gauge");
-
-function setGaugeValue(gauge, value) {
-	if (value < 0 || value > 1){
-
-	
-	return;
-}
-// For the value just do current water divided by water calories 
-// Set the value to the current water amount
-
-	gauge.querySelector(".gauge__fill").style.transform = `rotate(${value / 2}turn)`;
-	gauge.querySelector(".gauge__cover").textContent = `${Math.round(value * 100)}%`;
-
-}
-
-setGaugeValue(gaugeElement, 0.7);
 
 export default Water;
---
+++ client/src/components/waterAnimation/moving.js
@@ -0,0 +1,30 @@
+// import { useRef } from "react";
+
+// const GaugeWork = () =>  {
+//     // const gaugeFill = useRef(null);
+//     // const gaugeEmpty = useRef(null);
+
+//     const gaugeElement = document.querySelector(".gauge");
+
+// async function setGaugeValue(gauge, value) {
+    
+
+// 	if (value < 0 || value > 1){
+
+	
+// 	return;
+// }
+// // For the value just do current water divided by water calories 
+// // Set the value to the current water amount
+    
+// 	gauge.querySelector(".gauge__fill").style.transform = `rotate(${value / 2}turn)`;
+// 	gauge.querySelector(".gauge__cover").textContent = `${Math.round(value * 100)}%`;
+
+
+
+// }
+
+// setGaugeValue(gaugeElement, 0.3 );
+// }
+
+// export default GaugeWork;
