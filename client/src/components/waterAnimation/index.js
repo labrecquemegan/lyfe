5e96f3a2dfc3c0de1cb58f5a2fb1817fe67eb0ea
unknown <javi.mariscal13@gmail.com>
2022-03-14 10:06:28 -0400

    Finished Water Animation Basic

--- client/src/components/waterAnimation/index.js
+++ client/src/components/waterAnimation/index.js
@@ -1,14 +1,36 @@
 import React from "react";
 import "./waterAnimation.css";
 
+
 const Water = () => {
 	return (
 
-		<div class = "parent">
-        <div class = "child"></div>
-         </div>
+		<div class = "gauge">
+        <div class = "gauge__body">
+            <div class = "gauge__fill"></div>
+            <div class = "gauge__cover"></div>
+        </div>
+    </div>
 
 	);
 };
+
+const gaugeElement = document.querySelector(".gauge");
+
+function setGaugeValue(gauge, value) {
+	if (value < 0 || value > 1){
+
+	
+	return;
+}
+// For the value just do current water divided by water calories 
+// Set the value to the current water amount
+
+	gauge.querySelector(".gauge__fill").style.transform = `rotate(${value / 2}turn)`;
+	gauge.querySelector(".gauge__cover").textContent = `${Math.round(value * 100)}%`;
+
+}
+
+setGaugeValue(gaugeElement, 0.7);
 
 export default Water;
