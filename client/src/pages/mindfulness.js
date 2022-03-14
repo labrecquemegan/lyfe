6b7bc2afa94183a4960e23f0628cd7e456b22046
xndrbrgs <aborgescolon@gmail.com>
2022-03-14 16:05:23 -0400

    might have fixed css

--- client/src/pages/mindfulness.js
+++ client/src/pages/mindfulness.js
@@ -1,27 +1,13 @@
 import React from "react";
 import Calendar from '../components/Calendar/index';
-
-let style = {
-  display: 'flex',
-  flexWrap: 'wrap',
-  flexDirection: 'row',
-  alignContent: 'center',
-  alignItems: 'center',
-  justifyContent: 'center'
-}
+import YogaBars from '../components/Mindfulness/YogaBars'
 
 const Mindfulness = () => {
   return (
     <>
-    <div className='page'>
-      <div className='container' style={style}>
-        <div>
-        <Calendar  />
-      
-          <p>Mindful Annimation goes here</p>
-          </div>
+      <div className="page">
+         <YogaBars />
       </div>
-    </div>
     </>
   );
 };
