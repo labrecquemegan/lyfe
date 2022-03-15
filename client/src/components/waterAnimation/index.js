cea6175cfcf0c9aa5c51f709adea06df2388be0c
unknown <javi.mariscal13@gmail.com>
2022-03-15 09:55:04 -0400

    Finished water finally

--- client/src/components/waterAnimation/index.js
+++ client/src/components/waterAnimation/index.js
@@ -1,51 +1,49 @@
 import React, { useState } from 'react';
 import './waterAnimation.css';
-import "./moving.js";
+import { setGaugeValue } from "./moving.js";
 
 
 const Water = () => {
-	
+
 		const [data, setData] = useState(null);
 		const [print, setPrint] = useState(false);
+		const [spin, setSpin] = useState(0);
 
 		function getData(val) {
 			setData(val.target.value)
-			console.warn(val.target.value)
+			setPrint(false);
+			console.log(val.target.value);
+			setGaugeValue(val.target.value);
 		}
+
 		return (
 			<div className = "gauge">
 				<div className = "gauge__body"> 
-					<div className="gauge__fill"></div>
+					<div className="gauge__fill" ></div>
 
-					<div className="gauge__cover"></div>
-				</div>
-						
-				{
+					<div className="gauge__cover">
+
+					{
+						// If print is true 
 					print?
+					
 					<h1>{data}</h1>
 					:null
-				}
-				<input type = "text" onChange = {getData} />
+					}
+				
+					</div>
+				</div>
+						
+				{/* So when button is clicked put
+					the value of the number as the rotation */}
+
+
+				<input type = "number" max={50} onChange = {getData} />
 				<button onClick = {() => setPrint(true)} >Print Value</button>
+				
 				
 			</div>
 		)
-
-		// <div>
-		// 	<div className="gauge">
-		// 		<div className="gauge__body">
-		// 			<div className="gauge__fill"></div>
-
-		// 			<div className="gauge__cover"></div>
-		// 		</div>
-		// 	</div>
-		// 	<input type="text" className="water-amount" />
-		// 	<button>Submit</button>
-		// </div>
-		//Remember return()
-
-	
-
 };
 
 export default Water;
--- client/src/components/waterAnimation/moving.js
+++ client/src/components/waterAnimation/moving.js
@@ -1,32 +1,11 @@
-// const gaugeElement = document.querySelector(".gauge");
+export function setGaugeValue(value) { 
+            
+        //     if (value < 0 || value > 1){
 
-//  function setGaugeValue(gauge, value) {
-    
+        //     return;
+        // }
 
-// 	if (value < 0 || value > 1){
-
-	
-// 	return;
-// }
-// // For the value just do current water divided by water calories 
-// // Set the value to the current water amount
-    
-// 	gauge.querySelector(".gauge__fill").style.transform = `rotate(${value / 2}turn)`;
-// 	gauge.querySelector(".gauge__cover").textContent = `${Math.round(value * 100)}%`;
-
-
-
-// }
-
-// setGaugeValue(gaugeElement, 0.3 );
-
-import { useRef } from 'react';
-
-function SpinningCircle(gauge, value) {
-    const gaugeElement = useRef(null);
-    
-    
-    
-}
-
-export default SpinningCircle
+            document.querySelector(".gauge__fill").style.transform = `rotate(${value / 200}turn)`;
+            document.querySelector(".gauge__cover").textContent = `${Math.round(value)}%`;
+        
+        }
