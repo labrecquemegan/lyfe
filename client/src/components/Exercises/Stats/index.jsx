c921f251852d13e47973939a5ce30c0568cd1f10
Alex Widener <awidener3@gmail.com>
2022-03-15 12:35:00 -0400

    Began exercise stats display

--- client/src/components/Exercises/Stats/index.jsx
+++ client/src/components/Exercises/Stats/index.jsx
@@ -1,34 +1,39 @@
-import React from "react";
-import "./style.scss";
+import React from 'react';
+import './style.scss';
 
-export default function Stats() {
-  return (
-    <section className="stats-container">
-        <div className="row">
-          <h2>Your Exercise Statistics</h2>
-          <div className="btn-row">
-            <div className="stats">
-              <div className="stat-boxes">
-                <span>3</span>
-                <div className="days">
-                  <h3>Days Total</h3>
-                </div>
-              </div>
-              <div className="stat-boxes">
-                <span>4</span>
-                <div className="streak">
-                  <h3>Your Highest Streak</h3>
-                </div>
-              </div>
-              <div className="stat-boxes">
-                <span>5</span>
-                <div className="total">
-                  <h3>Days Total</h3>
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
-    </section>
-  );
+export default function Stats({ user }) {
+	console.log(user);
+	return (
+		<section className="stats-container">
+			<div className="row">
+				<div>
+					<div></div>
+				</div>
+				<h2>Your Exercise Statistics</h2>
+				<div className="btn-row">
+					<div className="stats">
+						<div className="stat-boxes">
+							<span>
+								{user.exercise_stats.todays_exercise_duration}
+							</span>
+							<div className="days">
+								<h3>Todays Duration</h3>
+							</div>
+						</div>
+						<div className="stat-boxes">
+							<span>
+								{user.exercise_stats.total_exercise_duration}
+							</span>
+							<div className="total">
+								<h3>Total Duration</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<h3 className="goal-div">
+					Your Exercise Goal: {user.exercise_goal} min/day
+				</h3>
+			</div>
+		</section>
+	);
 }
