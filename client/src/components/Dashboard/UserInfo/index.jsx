7546fe0d6bd9c0fe56b8c5d5d381f4747433f95d
xndrbrgs <aborgescolon@gmail.com>
2022-03-15 13:04:32 -0400

    user info boxes

--- client/src/components/Dashboard/UserInfo/index.jsx
+++ client/src/components/Dashboard/UserInfo/index.jsx
@@ -1,17 +1,34 @@
-import './userinfo.scss';
-import { AiFillCheckCircle } from 'react-icons/ai';
-import { IconContext } from 'react-icons';
+import "./userinfo.scss";
+import { AiFillCheckCircle } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 export default function UserInfo({ user }) {
-	console.log(user);
-	return (
-		<section className="userinfo-container">
-			<div className="row">
-                <h2>Your User Settings</h2>
-                <div className="user-profile">
-                    
-                </div>
-			</div>
-		</section>
-	);
+  console.log(user);
+  return (
+    <section className="userinfo-container">
+      <div className="row">
+        <h2>Your User Settings</h2>
+        <div className="user">
+          <div className="user-info">
+            <div className="user-row">
+              <h3>Weight</h3>
+              <input type="text" />
+            </div>
+            <div className="user-row">
+              <h3>Height</h3>
+              <input type="text" />
+            </div>
+            <div className="user-row">
+              <h3>Age</h3>
+              <input type="text" />
+            </div>
+            <div className="user-row">
+              <h3>Gender</h3>
+              <input type="text" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
