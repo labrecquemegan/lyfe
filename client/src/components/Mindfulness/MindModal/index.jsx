10100556144d2a4e85c1c7ecdfb4c8e68039c06a
xndrbrgs <aborgescolon@gmail.com>
2022-03-15 16:47:39 -0400

    modals mostly done

---
+++ client/src/components/Mindfulness/MindModal/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+import './mindmodal.scss'
+import MindfulnessInput from "../MindfulInput";
+
+const MindModal = ({ user }) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  return (
+    <div className="modal-row">
+      <button onClick={onOpenModal}>Log Your Mindful Activities</button>
+      <Modal 
+      open={open} 
+      onClose={onCloseModal} 
+      center
+      classNames={{
+          overlay: 'customOverlay',
+          modal: 'customModal'
+      }}>
+        <MindfulnessInput user={user}/>
+      </Modal>
+    </div>
+  );
+};
+
+export default MindModal;
---
+++ client/src/components/Nutrition/FoodModal/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+import FoodInput from '../FoodInput'
+import './foodmodal.scss'
+
+const FoodModal = ({ user }) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  return (
+    <div className="modal-row">
+      <button onClick={onOpenModal}>Log Your Meals</button>
+      <Modal 
+      open={open} 
+      onClose={onCloseModal} 
+      center
+      classNames={{
+          overlay: 'customOverlay',
+          modal: 'customModal'
+      }}>
+        <FoodInput user={user}/>
+      </Modal>
+    </div>
+  );
+};
+
+export default FoodModal;
