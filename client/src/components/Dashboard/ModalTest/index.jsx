f5dae87ae7637a04fe077457ca0fee8e8af88737
xndrbrgs <aborgescolon@gmail.com>
2022-03-15 15:55:13 -0400

    dash modal works

---
+++ client/src/components/Dashboard/ModalTest/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+import UserInfo from "../UserInfo";
+import './modal.scss'
+
+const ModalTest = ({ user }) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  return (
+    <div className="modal-row">
+      <button onClick={onOpenModal}>Change User Info</button>
+      <Modal 
+      open={open} 
+      onClose={onCloseModal} 
+      center
+      classNames={{
+          overlay: 'customOverlay',
+          modal: 'customModal'
+      }}>
+        <UserInfo user={user}/>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalTest;
