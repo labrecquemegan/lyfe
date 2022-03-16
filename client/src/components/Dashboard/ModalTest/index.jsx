7fb2c533bfa442bfb45daa9a157f3581442076a0
xndrbrgs <aborgescolon@gmail.com>
2022-03-16 11:31:44 -0400

    added effects to modals

--- client/src/components/Dashboard/ModalTest/index.jsx
+++ client/src/components/Dashboard/ModalTest/index.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 import UserInfo from '../UserInfo';
 import './modal.scss';
+import {gsap, Power3} from 'gsap';
 
 const ModalTest = ({ user }) => {
 	const [open, setOpen] = useState(false);
@@ -10,8 +11,23 @@ const ModalTest = ({ user }) => {
 	const onOpenModal = () => setOpen(true);
 	const onCloseModal = () => setOpen(false);
 
+	let UserAnim = useRef(null);
+  console.log(UserAnim);
+
+  useEffect(() => {
+    console.log(UserAnim);
+    gsap.to(UserAnim, 3.5, {
+      opacity: 1,
+      y: 40,
+      ease: Power3.easeOut,
+    });
+  }, []);
+
+
 	return (
-		<div className="dashboard-modal-row">
+		<div className="dashboard-modal-row" ref={(container) => {
+			UserAnim = container;
+		  }}>
 			<button onClick={onOpenModal}>Change User Info</button>
 			<Modal
 				open={open}
--- client/src/components/Exercises/ExerModal/index.jsx
+++ client/src/components/Exercises/ExerModal/index.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 import './exermodal.scss';
 import ExerciseInput from '../ExerInput';
+import {gsap, Power3} from 'gsap';
 
 const ExerModal = ({ user }) => {
 	const [open, setOpen] = useState(false);
@@ -10,8 +11,22 @@ const ExerModal = ({ user }) => {
 	const onOpenModal = () => setOpen(true);
 	const onCloseModal = () => setOpen(false);
 
+	let UserAnim = useRef(null);
+	console.log(UserAnim);
+  
+	useEffect(() => {
+	  console.log(UserAnim);
+	  gsap.to(UserAnim, 3.5, {
+		opacity: 1,
+		y: 40,
+		ease: Power3.easeOut,
+	  });
+	}, []);
+
 	return (
-		<div className="exercise-modal-row">
+		<div className="exercise-modal-row" ref={(container) => {
+			UserAnim = container;
+		  }}>
 			<button onClick={onOpenModal}>Log Exercise</button>
 			<Modal
 				open={open}
